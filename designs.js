798afb8d30bf734ada949e2c63fc6dd193d85dc0
Unknown <dezsoj@gmail.com>
2018-02-07 14:48:42 +0100

    remove console.logs and creating finction call for makeGrid

--- designs.js
+++ designs.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
+    var choosenColor;
     // Select color input
     $('#colorPicker').on('change', function() {
         var choosenColor = this.value;
-        console.log(choosenColor);
+        // console.log(choosenColor);
     });
+
     // Select size input
     $("#sizePicker").submit(function(event) {
         event.preventDefault();
         var height = $("input[name='height']", this).val();
         var width = $("input[name='width']", this).val();
-        console.log("height: " + height + " | width: " + width);
+        // console.log("height: " + height + " | width: " + width);
+        makeGrid(height,width);
     });
+
     // When size is submitted by the user, call makeGrid()
-
-    function makeGrid() {
-
-        // Your code goes here!
+    // Your code goes here!
+    function makeGrid(height,width) {
 
     }
 
